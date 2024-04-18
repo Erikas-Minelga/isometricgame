afc72f6c64350d89e57205463e516d3fdbc9d1ae
Erikas Minelga <erikas.minelga96@gmail.com>
2024-04-18 07:38:07 +0100

    Fixed walking. Added walk and idle animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,28 +6,48 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 3.0f;
-    private float _movementX, _movementY;
-    private Input _input;
-    //private Rigidbody2D _rb;
+
+    private Vector2 _movement;
+    private Rigidbody2D _rb;
+    private Animator _anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //_rb ??= GetComponent<Rigidbody2D>();
-        _input = new Input();
-        _input.Enable();
-        _input.Ground.MoveX.performed += moving => {
-            _movementX = moving.ReadValue<float>();
-        };
-        _input.Ground.MoveY.performed += moving => {
-            _movementY = moving.ReadValue<float>();
-        };
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+
+        _movement = new Vector2(0f,0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(_movementX * speed * Time.deltaTime, _movementY * speed * Time.deltaTime, 0));
-        //_rb.velocity = new Vector2(_movementX * speed, _movementY * speed);
+        Move();
+        Animate();
+    }
+
+    void Move()
+    {
+        _movement.x = Input.GetAxis("Horizontal") * speed;
+        _movement.y = Input.GetAxis("Vertical") * speed;
+        _rb.velocity = _movement;
+    }
+
+    void Animate()
+    {
+        _anim.SetFloat("MovementX", _movement.x);
+        _anim.SetFloat("MovementY", _movement.y);
+        _anim.SetFloat("MovementMag", _movement.magnitude);
+
+        float rawAxisHor = Input.GetAxisRaw("Horizontal");
+        float rawAxisVer = Input.GetAxisRaw("Vertical");
+
+        if(rawAxisHor == 1 || rawAxisHor == -1 || rawAxisVer == 1 || rawAxisVer == -1)
+        {
+            _anim.SetFloat("LastX", rawAxisHor);
+            _anim.SetFloat("LastY", rawAxisVer);
+        }
     }
 }
